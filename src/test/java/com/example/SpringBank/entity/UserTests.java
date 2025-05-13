ddd05d44da69ce28054d4185228ca5ea0e725fc6
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-05-13 09:01:23 -0400

    add comments for the unit test classes for User class

--- src/test/java/com/example/SpringBank/entity/UserTests.java
+++ src/test/java/com/example/SpringBank/entity/UserTests.java
@@ -6,12 +6,21 @@ import static com.example.SpringBank.Helpers.*;
 
 @SpringBootTest
 public class UserTests {
+    /**
+     * Ensures the User entity class has the fields for login credentials.
+     * @result Test will pass if User class contains the field <code>username</code> and <code>passwordHash</code> of
+     *          string type. Otherwise, the test will fail.
+     */
     @Test
     void hasLoginCredentials() {
         checkClassHasField(User.class, "username", String.class);
         checkClassHasField(User.class, "passwordHash", String.class);
     }
 
+    /**
+     * Ensures the User entity class is abstract.
+     * @result Test will pass if User class is abstract, but will fail if User class is not.
+     */
     @Test
     void userClassIsAbstract() {
         checkClassIsAbstract(User.class);
